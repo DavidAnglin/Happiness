c981a8fad018859018cfde35c023fbac12673411
[David Anglin] <[DAAnglin6389@gmail.com]>
2016-02-04 13:32:30 -0800

    Created bezierpaths, faceView, Face is working

---
+++ Hapiness/HappinessViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HappinessViewController.swift
+//  Hapiness
+//
+//  Created by David Anglin on 2/4/16.
+//  Copyright © 2016 David Anglin. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+  
+
+}
